729ba2fd594a69f1264a46152afba6ed468f4114
Rubén Villacreces Vazquez <rvillacr@usersad.everis.int>
2017-03-22 00:32:12 +0100

    actualizacion

--- test1.py
+++ test1.py
@@ -1,6 +1,1 @@
-import sys
-f = open("fichero_nuevo.txt","w") #opens file with name of "fichero_nuevo.txt"
-f.write("linea 1.\n")
-f.write("linea 2.\n")
-f.write("linea 3.\n")
-f.close()
+import this
